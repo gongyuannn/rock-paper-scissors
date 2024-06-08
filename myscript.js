1b750cfb01594f4e20073c7a751d3f5be6f88756
GY <104050074+gongyuannn@users.noreply.github.com>
2024-06-08 21:16:12 +0800

    Add logic for playing a single round, when user choses rock

--- myscript.js
+++ myscript.js
@@ -20,36 +20,74 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-console.log(getComputerChoice());
-
 // Step 2. Human Choice
 // Create function for Human Choice
 function getHumanChoice(){
 // Prompt user input
+// TO FIX: Check that the input is valid - only 1("rock"), 2("paper") or 3("scissors") - Use while 'true' loop
+
     while(true){
-        let humanChoice = prompt(`Enter a number from 1 to 3. 
-        1 - rock
-        2 - paper
-        3 - scissors`)
+        let humanChoice = prompt(`Enter one of the following:
+        - rock
+        - paper
+        - scissors`).toLowerCase();
+ // If valid, store the input into a variable
         switch(humanChoice){
-            case "1":
+            case "rock":
                 return humanChoice = "rock";
                 break;
-            case "2":
+            case "paper":
                 return humanChoice = "paper";
                 break;
-            case "3":
+            case "scissors":
                 return humanChoice = "scissors";
                 break;
+// If invalid input, prompt user to enter the input again until valid
             default:
                 alert("Please enter a valid number.");
          }
     }
 }
 
-// Check that the input is valid - only 1("rock"), 2("paper") or 3("scissors") - Use while 'true' loop
+// Step 3: Declare player score variables
+// Create a variable in global scope to store human score
+let humanScore = 0;
+// Initialize to 0
+// Create a variable in global scope to store computer score
+// Initialize to 0 
+let computerScore = 0;
 
+// Step 4: Write logic to play a single round
+// Create a function that takes in human and computer choices as parameters
+function playRound(humanChoice, computerChoice){
+    if(humanChoice=="rock" && computerChoice=="scissors"){
+        console.log("Computer chose scissors.");
+        console.log("You win! Rock beats scissors!");
+        humanScore++;
+    } else if(humanChoice=="rock"&& computerChoice=="paper"){
+        console.log("Computer chose paper.");
+        console.log("You lose! Paper beats rock!");
+        computerScore++;
+    } else if(humanChoice=="rock"&& computerChoice=="rock"){
+        console.log("Computer chose rock.");
+        console.log("It's a tie! Go again");
+    }
 
-// If valid, store the input into a variable
-// If invalid input, prompt user to enter the input again until valid
-getHumanChoice();
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+
+// Ensure that human choice parameter is case-insensitive
+// Compare human and computer choice
+// If human and computer is same, it is a tie. Go again.
+// If human is rock, and computer is scissors, human wins. Add 1 to human score.
+// If human is rock, and computer is paper, human loses. Add 1 to computer score.
+// If human is scissors, and computer is paper, human wins. Add 1 to human score.
+// If human is scissors, and computer is rock, human loses. Add 1 to computer score.
+// If human is paper, and computer is rock, human wins. Add 1 to human score.
+// If human is paper, and computer is scissors, human loses. Add 1 to computer score.
+
+// Step 5: Write logic to play 5 rounds
+// Create a function to play 5 rounds
+// Move playRound function to playGame function
+// Call playRound function 5 times
